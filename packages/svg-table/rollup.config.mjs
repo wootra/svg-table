763ced9c23defb3ebc26a4da58f5533a1f681407
Songhyeon Jun <shjeon0730@gmail.com>
2024-07-04 23:33:48 -0500

    update bundling package

---
+++ packages/svg-table/rollup.config.mjs
@@ -0,0 +1,38 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import terser from '@rollup/plugin-terser';
+import pkg from './package.json' assert { type: 'json' };
+
+export default [
+	{
+		input: 'src/index.ts',
+		output: [
+			{
+				file: pkg.main,
+				format: 'cjs',
+				sourcemap: true,
+			},
+			{
+				file: pkg.module,
+				format: 'es',
+				sourcemap: true,
+			},
+			{
+				name: '@shjeon0730/svg-table',
+				file: pkg.browser,
+				format: 'umd',
+				sourcemap: true,
+			},
+		],
+		external: ['react', 'react-dom'],
+		plugins: [
+			resolve(),
+			commonjs(),
+			peerDepsExternal(),
+			typescript({ tsconfig: './tsconfig.json' }),
+			terser(),
+		],
+	},
+];
