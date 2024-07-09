b994355be5a4dd506650709339f34d95e1db34d7
Songhyeon Jun <shjeon0730@gmail.com>
2024-07-09 01:14:39 -0500

    update 2.0.0 api document

--- apps/docs/src/pages/api-ref/(utils)/_VersionNav.tsx
+++ apps/docs/src/pages/api-ref/(utils)/_VersionNav.tsx
@@ -1,4 +1,12 @@
-const versions = ['1.3.0', '1.4.0', '1.5.0', '1.6.0', '1.7.0', '1.8.0']; // Add or modify versions as needed
+const versions = [
+	'1.3.0',
+	'1.4.0',
+	'1.5.0',
+	'1.6.0',
+	'1.7.0',
+	'1.8.0',
+	'2.0.0',
+]; // Add or modify versions as needed
 
 const VersionNav = () => {
 	const selected =
