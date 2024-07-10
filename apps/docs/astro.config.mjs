0af6107b9741ccbde4ae11c6f007c7646b79eacc
Songhyeon Jun <shjeon0730@gmail.com>
2024-07-10 14:07:27 -0500

    update site to production url

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -6,5 +6,5 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
 	integrations: [react(), mdx()],
-	site: `https://${process.env.VERCEL_URL}`, // 'https://svg-table.vercel.com',
+	site: `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`, // 'https://svg-table.vercel.com',
 });
