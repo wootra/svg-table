b34f5925c81000975fd6ba3d1ae95b024acf0469
Songhyeon Jun <shjeon0730@gmail.com>
2024-07-14 00:54:28 -0500

    update d.ts generation logic

--- packages/svg-table-react/rollup.config.dts.mjs
+++ packages/svg-table-react/rollup.config.dts.mjs
@@ -2,6 +2,10 @@ import dts from 'rollup-plugin-dts';
 
 export default {
 	input: './out/types/svg-table-react/src/index.d.ts',
-	output: [{ file: 'dist/index.d.ts', format: 'es' }],
+	output: [
+		{ file: 'dist/index.d.ts', format: 'es' },
+		{ file: 'dist/index.mjs.d.ts', format: 'es' },
+		{ file: 'dist/index.umd.d.ts', format: 'es' },
+	],
 	plugins: [dts()],
 };
--- packages/svg-table-solid/rollup.config.dts.mjs
+++ packages/svg-table-solid/rollup.config.dts.mjs
@@ -1,7 +1,11 @@
 import dts from 'rollup-plugin-dts';
 
 export default {
-	input: './out/types/svg-table-vanilla/src/index.d.ts',
-	output: [{ file: 'dist/index.d.ts', format: 'es' }],
+	input: './out/types/svg-table-react/src/index.d.ts',
+	output: [
+		{ file: 'dist/index.d.ts', format: 'es' },
+		{ file: 'dist/index.mjs.d.ts', format: 'es' },
+		{ file: 'dist/index.umd.d.ts', format: 'es' },
+	],
 	plugins: [dts()],
 };
