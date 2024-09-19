ac6df8441be829561934c9fdcdd9e364bc0c634e
Songhyeon Jun <shjeon0730@gmail.com>
2024-09-19 08:24:27 -0500

    build script update

--- packages/svg-table-solid/publish.sh
+++ packages/svg-table-solid/publish.sh
@@ -8,7 +8,7 @@ fi
 echo "version is $VERSION"
 
 pnpm run lint && echo 'lint success' || exit 1
-pnpm build && echo 'build success'|| exit 1
+pnpm rebuild && echo 'build success'|| exit 1
 echo $VERSION
 npm version $VERSION
 
